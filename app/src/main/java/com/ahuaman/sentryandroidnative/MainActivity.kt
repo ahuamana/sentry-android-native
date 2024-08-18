d144f50109062e6d63c733cf5cce22d0ebe00aec
Antony Nicolas Huaman Alikhan <73962071@continental.edu.pe>
2024-08-18 14:55:55 -0500

    first commit

---
+++ app/src/main/java/com/ahuaman/sentryandroidnative/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.ahuaman.sentryandroidnative
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ahuaman.sentryandroidnative.ui.theme.SentryAndroidNativeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SentryAndroidNativeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HomeScreen(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello, World!",
+        modifier = modifier
+    )
+    throw RuntimeException("This is a test exception - please ignore")
+}
